84db7729082d6b81d7702931b8015c1440ee3210
I-zyon-I <81296195+I-zyon-I@users.noreply.github.com>
2021-03-28 21:48:27 +0200

    Add files via upload

---
+++ controllers/client_controller.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "../vendor/autoload.php";
+
+$pdo = new PDO("sqlite:../bdd-sqlite/thalasso.db");
+
+$faker = Faker\Factory::create('fr_FR');
+ 
+for ($i = 0; $i < 10; $i++) {
+    $statement = $pdo->prepare("INSERT INTO client (nomClient, prenomClient) VALUES (:nomClient, :prenomClient)");
+    $statement->execute(array(
+        'nomClient' => $faker->lastname(),
+        'prenomClient' => $faker->firstname()
+    ));
+};
+
+
+
+$statement = $pdo->query("select * from client");
+$result = $statement->fetchAll();
+print_r($result);
+
+?>
