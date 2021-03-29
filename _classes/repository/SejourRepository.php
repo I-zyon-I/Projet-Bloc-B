f5c3f6643c7f5a8583f63f080d1d831c4fb5974a
Arnaud-code <81296269+Arnaud-code@users.noreply.github.com>
2021-03-29 10:40:05 +0200

    Update SejourRepository.php

--- _classes/repository/SejourRepository.php
+++ _classes/repository/SejourRepository.php
@@ -1,26 +1,48 @@
 <?php
 
-class SejourRepository
-{
+class SejourRepository {
     // Attributs
     protected $pdo;
 
     // Constructeur
-    public function __construct(PDO $pdo)
-    {
+    public function __construct(PDO $pdo) {
         $this->pdo = $pdo;
     }
 
     // Méthode
-    public function findAll()
-    {
+    public function findAll() {
         try {
-            return $this->pdo->query("select * from Sejour");
+            return $this->pdo->query("SELECT
+            sj.idSejour AS idSejour,
+            sj.dateDebutSejour AS dateDebutSejour,
+            sj.dureeJourSejour AS dureeJourSejour,
+            sj.vestiaireSejour AS vestiaireSejour,
+            sj.statutSejour AS statutSejour,
+            cl.nomClient AS nomClient,
+            cl.prenomClient AS prenomClient,
+            cl.naissanceClient AS naissanceClient,
+            cl.mailClient AS mailClient,
+            sc.dateSeance AS dateSeance,
+            sC.heureSeance AS heureSeance,
+            so.nomSoin AS nomSoin,
+            so.dureeMinuteSoin AS dureeMinuteSoin,
+            ep.nomEspace AS nomEspace
+        FROM Sejour AS sj
+        LEFT JOIN Client AS cl
+            ON sj.idClient = cl.idClient
+        LEFT JOIN Seance AS sc
+            ON sj.idSejour = sc.idSejour
+        LEFT JOIN Soin AS so
+            ON sc.idSoin = so.idSoin
+        LEFT JOIN Espace as ep
+            ON so.idEspace = ep.idEspace
+        ORDER BY
+            sj.dateDebutSejour ASC,
+            sj.idSejour ASC,
+            sc.dateSeance ASC,
+            sc.heureSeance ASC;");
         } catch (PDOException $e) {
             echo "Erreur Query sur : " . $e->getMessage();
         }
     }
-
 }
-
-?>
