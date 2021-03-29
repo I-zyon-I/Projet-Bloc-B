7d80f8e943224287ffe70c6b0ec2d12e6a323f5f
I-zyon-I <81296195+I-zyon-I@users.noreply.github.com>
2021-03-29 10:48:23 +0200

    Add files via upload

---
+++ views/home_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php 
+    require "includes/_head.php"
+?>
+<link rel="stylesheet" href="../assets/css/mvc.css">
+</head>
+
+<body>
+
+<?php 
+    require "includes/_nav.php";
+    require "includes/main_home.php";
+    require "includes/_footer.php";
+    require "includes/_script.php";
+?>
+    
+</body>
+</html>
---
+++ views/includes/main_home.php
@@ -0,0 +1,11 @@
+<main role="main" class="container-fluid">
+    <header>
+        
+    
+    </header>
+    <article>
+    
+    
+    </article>
+    
+</main>
